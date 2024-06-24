234a5402e0c5c85a0ad404bba863a051e4ef22a0
joyline-cheruto <joycheruto832@gmail.com>
2024-06-24 16:37:28 +0300

    used string formatter

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,8 +1,7 @@
 import random
-print("Your Password: ")
 chars = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*_?"
 password = ""
 for x in range(16):
     password += random.choice(chars)
 
-print(password)
+print(f"Your Password is: {password}")
