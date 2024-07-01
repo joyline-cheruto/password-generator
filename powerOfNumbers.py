52c431a623e2224ac598b34868b31f707d1f1e4d
joyline-cheruto <joycheruto832@gmail.com>
2024-07-01 15:21:50 +0300

    eliminating the if statement

--- powerOfNumbers.py
+++ powerOfNumbers.py
@@ -1,12 +1,8 @@
 def power_of_num(base_num, power_num):
     result = 1
-    if power_num == 0:
-        return result
-    if power_num == 1:
-        return base_num
-    for i in range(0, power_num):
+    for i in range(power_num):
         result = result * base_num
     return result
 
 
-print(power_of_num(3, 4))
+print(power_of_num(3, 0))
